a3c839e2f2a929219650f72a1393526015217091
顾咏 <zhijiang.nzj@alibaba-inc.com>
2019-03-23 17:03:28 +0800

    Update Apis

--- V20170525/Api.php
+++ V20170525/Api.php
@@ -2,7 +2,20 @@
 
 namespace AlibabaCloud\Dysmsapi\V20170525;
 
+use AlibabaCloud\ApiResolverTrait;
 use AlibabaCloud\Rpc;
+
+/**
+ * Resolve Api based on the method name.
+ *
+ * @method SendBatchSms sendBatchSms(array $options = [])
+ * @method QuerySendDetails querySendDetails(array $options = [])
+ * @method SendSms sendSms(array $options = [])
+ */
+class DysmsapiApiResolver
+{
+    use ApiResolverTrait;
+}
 
 class V20170525Rpc extends Rpc
 {
